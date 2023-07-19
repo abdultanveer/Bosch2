53c80db655b22970299aab84c96c49f91e929b78
Abdul <abdul.tanveer@gmail.com>
2023-07-19 13:12:05 +0300

    lifecycle of an activity and logcat

--- app/src/main/java/com/example/bosch/HomeActivity.java
+++ app/src/main/java/com/example/bosch/HomeActivity.java
@@ -5,18 +5,22 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+
+    public static String TAG = HomeActivity.class.getSimpleName();
     TextView conTextView;
     EditText conEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home); //inflation -- baloon --layoutinflater
+        Log.i(TAG,"im in oncreate");
       //  inflation();
         conTextView = findViewById(R.id.tvContact);  //taking handle on the textview
         conEditText = findViewById(R.id.etContact);
@@ -28,6 +32,41 @@ public class HomeActivity extends AppCompatActivity {
 
         //put that string onto the textview
         conTextView.setText(data);*/
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG,"im in onstart-- user interacting");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w(TAG,"im in onpause-- save state of app");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"im in onresume -- restore the state app");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG,"im in onstop");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,"im in ondestroy -- release resources");
+
     }
 
     private void inflation() {
