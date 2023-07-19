0852d1dbd732538bc70d079405571b976af26552
Abdul <abdul.tanveer@gmail.com>
2023-07-19 12:36:39 +0300

    start activity for result

--- app/src/main/java/com/example/bosch/HomeActivity.java
+++ app/src/main/java/com/example/bosch/HomeActivity.java
@@ -3,27 +3,31 @@ package com.example.bosch;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
     TextView conTextView;
+    EditText conEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home); //inflation -- baloon --layoutinflater
       //  inflation();
         conTextView = findViewById(R.id.tvContact);  //taking handle on the textview
+        conEditText = findViewById(R.id.etContact);
 
         //get the intent which started this activity
         //from that intent get the extras
         //get the string extra with key makey
-        String data =  getIntent().getExtras().getString("makey");
+      /*  String data =  getIntent().getExtras().getString("makey");
 
         //put that string onto the textview
-        conTextView.setText(data);
+        conTextView.setText(data);*/
     }
 
     private void inflation() {
@@ -34,5 +38,16 @@ public class HomeActivity extends AppCompatActivity {
         TextView conTextView = new TextView(this);
         ConstraintLayout constraintLayout = new ConstraintLayout(this);
         constraintLayout.addView(conedEditText);
+    }
+
+    public void handleContact(View view) {
+        //get the data from edittext
+        String phno = conEditText.getText().toString();
+        //put the data into an intent
+        Intent pIntent = new Intent();
+        pIntent.putExtra("phnum",phno);
+        setResult(RESULT_OK,pIntent);
+        //finish this activity
+        finish();
     }
 }
--- app/src/main/java/com/example/bosch/MainActivity1.java
+++ app/src/main/java/com/example/bosch/MainActivity1.java
@@ -2,9 +2,12 @@ package com.example.bosch;
 
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.provider.AlarmClock;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
@@ -19,9 +22,47 @@ public class MainActivity1 extends AppCompatActivity {
     }
 
     public void clickHandler(View view) {
-        Toast.makeText(this, "welcome to android", Toast.LENGTH_SHORT).show();
-        Intent homeIntent = new Intent(this, HomeActivity.class);
-        homeIntent.putExtra("makey","android bosch automotive");
-        startActivity(homeIntent);
+        startHome();
+        /*switch (view.getId()){
+            case R.id.btnSubmit:
+                startHome();
+
+                break;
+            case R.id.btnDial:*/
+               /* Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:12345678"));
+                startActivity(dialIntent);*/
+
+       // createAlarm("demo alarm",22,43);
+      /*  Intent myIntent = new Intent("ineed.water"); //implicit intent
+        startActivity(myIntent);*/
+
+
+
+    }
+
+    private  void startHome() {
+       // Toast.makeText(this, "welcome to android", Toast.LENGTH_SHORT).show();
+        //explicit intent
+        Intent homeIntent = new Intent(this, HomeActivity.class);  //my intention is to launch homeactivity
+      // homeIntent.putExtra("makey","android bosch automotive");  //data
+        startActivityForResult(homeIntent,123);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        String phno = data.getStringExtra("phnum");
+        TextView resView = findViewById(R.id.tvResult);
+        resView.setText(phno);
+    }
+
+    public void createAlarm(String message, int hour, int minutes) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_MESSAGE, message)
+                .putExtra(AlarmClock.EXTRA_HOUR, hour)
+                .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
