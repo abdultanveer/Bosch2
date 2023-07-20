5d58a6a54c5f8a6479116615141f82abc82a0dd0
Abdul <abdul.tanveer@gmail.com>
2023-07-20 08:43:32 +0300

    recyclerview part-2

--- app/src/main/java/com/example/bosch/CountriesAdapter.java
+++ app/src/main/java/com/example/bosch/CountriesAdapter.java
@@ -1,6 +1,52 @@
 package com.example.bosch;
 
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class CountriesAdapter extends RecyclerView.Adapter<CViewHolder> {
+import org.w3c.dom.Text;
+
+public class CountriesAdapter extends RecyclerView.Adapter<CountriesAdapter.CViewHolder> {
+    public static String TAG = CountriesAdapter.class.getSimpleName();
+    String[] mCountries;
+    public CountriesAdapter(String[] countries) {
+        mCountries = countries;
+    }
+
+    @NonNull
+    @Override
+    public CViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.i(TAG,"arvind bought a row plank from market and reserving in the holder");
+        View rowPlank = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_layout,parent,false);
+        return new CViewHolder(rowPlank);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CViewHolder holder, int position) {
+        Log.i(TAG,"keerthana is writing -- "+mCountries[position]);
+        holder.rowTextView.setText(mCountries[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.i(TAG,"abhishek counted -- "+mCountries.length);
+
+        return mCountries.length;
+    }
+
+    class CViewHolder extends RecyclerView.ViewHolder{
+    TextView rowTextView;
+        public CViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Log.i(TAG,"varsha received row plank");
+
+            rowTextView = itemView.findViewById(R.id.tvRow);
+        }
+    }
 }
--- app/src/main/java/com/example/bosch/RecyViewActivity.java
+++ app/src/main/java/com/example/bosch/RecyViewActivity.java
@@ -1,6 +1,7 @@
 package com.example.bosch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
@@ -14,6 +15,10 @@ public class RecyViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recy_view);
         RecyclerView countriesRecyclerView = findViewById(R.id.countrylistview);
+        CountriesAdapter adapter = new CountriesAdapter(countries);
+
+        countriesRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        countriesRecyclerView.setAdapter(adapter);
 
     }
 }
