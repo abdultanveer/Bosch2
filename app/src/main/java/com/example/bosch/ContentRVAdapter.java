52fd5ca4220c005cdef32ba014c90ffc28085d2f
Abdul <abdul.tanveer@gmail.com>
2023-09-02 14:43:55 +0530

    content provider for sms body and phno

--- app/src/main/java/com/example/bosch/ContentRVAdapter.java
+++ app/src/main/java/com/example/bosch/ContentRVAdapter.java
@@ -12,9 +12,11 @@ import androidx.recyclerview.widget.RecyclerView;
 public class ContentRVAdapter extends RecyclerView.Adapter<ContentRVAdapter.ContentVH> {
 Cursor mCursor;
 int colIndexBody ; //index of column name
+    int colIndexPhno;
     public ContentRVAdapter(Cursor cursor) {
         mCursor = cursor;
          colIndexBody = mCursor.getColumnIndexOrThrow("body"); //index of column name
+        colIndexPhno = mCursor.getColumnIndexOrThrow("address");
 
     }
 
@@ -29,6 +31,8 @@ int colIndexBody ; //index of column name
     public void onBindViewHolder(@NonNull ContentVH holder, int position) {
         mCursor.moveToPosition(position);
         holder.tvRowItem.setText(mCursor.getString(colIndexBody));
+        holder.phnoView.setText(mCursor.getString(colIndexPhno));
+
     }
 
     @Override
@@ -38,9 +42,11 @@ int colIndexBody ; //index of column name
 
     class  ContentVH extends RecyclerView.ViewHolder{
         TextView tvRowItem;
+        TextView phnoView;
         public ContentVH(@NonNull View itemView) {
             super(itemView);
             tvRowItem = itemView.findViewById(R.id.tvRowItem);
+            phnoView = itemView.findViewById(R.id.tvPhno);
         }
     }
 }
